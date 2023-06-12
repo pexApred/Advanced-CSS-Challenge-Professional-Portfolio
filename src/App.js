39b8d6b143a211790d07d78261ae156e81177d30
pexApred <120075705+pexApred@users.noreply.github.com>
2023-06-11 23:21:19 -0400

    setting up src files and components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Navigation />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
