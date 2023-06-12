e7f8492c5ba052ce920c6888b4b2927a94707b67
pexApred <120075705+pexApred@users.noreply.github.com>
2023-06-12 09:02:45 -0400

    working on file and folder structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,54 +5,41 @@ function Header() {
     return (
         <header className="header">
             <div class="icons">
-            <i class="devicon-javascript-plain"></i>
+                <i class="devicon-javascript-plain"></i>
 
-            <i class="devicon-nodejs-plain"></i>
+                <i class="devicon-nodejs-plain"></i>
 
-            <i class="devicon-mysql-plain-wordmark"></i>
+                <i class="devicon-mysql-plain-wordmark"></i>
 
-            <i class="devicon-css3-plain"></i>
+                <i class="devicon-css3-plain"></i>
 
-            <i class="devicon-html5-plain"></i>
+                <i class="devicon-html5-plain"></i>
 
-            <i class="devicon-handlebars-plain-wordmark"></i>
+                <i class="devicon-handlebars-plain-wordmark"></i>
 
-            <i class="devicon-express-original"></i>
+                <i class="devicon-express-original"></i>
 
-            <i class="devicon-eslint-original"></i>
+                <i class="devicon-eslint-original"></i>
 
-            <i class="devicon-git-plain"></i>
+                <i class="devicon-git-plain"></i>
 
-            <i class="devicon-jest-plain"></i>
+                <i class="devicon-jest-plain"></i>
 
-            <i class="devicon-npm-original-wordmark"></i>
+                <i class="devicon-npm-original-wordmark"></i>
 
-            <i class="devicon-npm-original-wordmark"></i>
+                <i class="devicon-npm-original-wordmark"></i>
 
-            <i class="devicon-react-original-wordmark"></i>
+                <i class="devicon-react-original-wordmark"></i>
 
-            <i class="devicon-sequelize-plain"></i>
+                <i class="devicon-sequelize-plain"></i>
 
-            <i class="devicon-vscode-plain"></i>
+                <i class="devicon-vscode-plain"></i>
 
-            <i class="devicon-bulma-plain"></i>
+                <i class="devicon-bulma-plain"></i>
 
-            <i class="devicon-canva-original"></i>
+                <i class="devicon-canva-original"></i>
 
-        </div>
-
-        <nav>
-            <h1><span href="#page-top" class="name">Emmanuel Lakis </span></h1>
-
-            <ul class="nav-group" role="navigation">
-                <li><a href="#page-top" class="nav-link">Home</a></li>
-                <li><a href="#life" class="nav-link">About</a></li>
-                <li><a href="#work" class="nav-link">Work</a></li>
-                <li><a href="#contact-me" class="nav-link">Contact</a></li>
-                <li><a href="#resume" class="nav-link">Resume</a></li>
-            </ul>
-
-        </nav>
+            </div>
         </header>
     );
 }
